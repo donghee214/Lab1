25aec2db77f45229f47d7cfe6c56dcce4be36ed7
Daniel Kim <danman.kim@mail.utoronto.ca>
2019-09-17 22:10:11 -0400

    commit

--- warmup/wc.c
+++ warmup/wc.c
@@ -24,7 +24,7 @@ wc_init(char *word_array, long size)
 	struct wc *wc;
 	char word[256] = "";
 	for(int i = 0; i < size; i++){
-		if(isspace(word_array[i] == 0)){
+		if(isspace(word_array[i]) == 0){
 			word[strlen(word)] = word_array[i];
 		}
 		else{
