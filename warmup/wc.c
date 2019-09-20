f18a1c13bb710b2324872dd1282bcaf77cd8ecc8
Daniel Kim <danman.kim@mail.utoronto.ca>
2019-09-20 00:27:38 -0400

    test

--- warmup/wc.c
+++ warmup/wc.c
@@ -26,7 +26,7 @@ hash(char *str)
     long hash = 5381;
     int c;
 
-    while (c = *str++)
+    while ((c = *str++))
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
     return hash;
