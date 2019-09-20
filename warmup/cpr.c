3716e631c58eda67850d3dc08398b9d7c8e00b02
Donghee Daniel Kim <danman.kim@mail.utoronto.ca>
2019-09-19 20:24:30 -0400

    commit

--- warmup/cpr.c
+++ warmup/cpr.c
@@ -1,5 +1,8 @@
 #include "common.h"
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 /* make sure to use syserror() when a system call fails. see common.h */
 
 void
@@ -15,6 +18,15 @@ main(int argc, char *argv[])
 	if (argc != 3) {
 		usage();
 	}
-	TBD();
-	return 0;
+        char* copyPath = argv[1];
+        char* destPath = argv[2];
+        int copyFile, destFile;
+        copyFile = open(copyPath, O_RDONLY);
+        destFile = open(destPath, O_RDWR|O_CREAT, 0666);
+        char buf[4096];
+        
+        int ret = read(copyFile, buf, 4096);
+        printf(buf);
+        write(destFile,buf, 4096);
+	return ret;
 }
--- warmup/wc.c
+++ warmup/wc.c
@@ -4,104 +4,158 @@
 #include <string.h>
 #include <stdlib.h>
 #include "common.h"
+#include <math.h>
 #include "wc.h"
 
-#define SIZE 1000000
+typedef struct {
+    char* key;
+    char* value;
+} dataItem;
 
-struct dataItem {
-	int data;
-	int key;
+struct wc{
+    long size;
+    int filledCount;
+    dataItem** items;
 };
 
-struct dataItem* wc[SIZE];
+static dataItem deletedDataItem = {NULL, NULL};
 
-int hashCode(int key) {
-   return key % SIZE;
+
+
+static int hash(const char* str, const int prime, const int buckets){
+    long hash = 0;
+    for (int i = 0; i < strlen(str); i++){
+        hash += (long)pow(prime, strlen(str) - (i+1)) * str[i];
+        hash = hash % buckets;
+    }
+    return (int)hash;
+};
+
+static int getHash(const char* str, const int attempt, const int buckets){
+    const int hashA = hash(str, 151, buckets);
+    const int hashB = hash(str, 163, buckets );
+    return (hashA + (attempt * (hashB + 1))) % buckets;
+};
+
+static dataItem* newDataItem(const char*inputKey, const char* inputValue){
+    dataItem* i = malloc(sizeof(dataItem));
+    i->key = strdup(inputKey);
+    i->value = strdup(inputValue);
+    return i;
+};
+
+void insert(struct wc* wc, const char* key, const char* value){
+    dataItem* item = newDataItem(key, value);
+    int index = getHash(item->key, 0, wc->size);
+    dataItem* currItem = wc->items[index];
+    int i = 1;
+    while(currItem != NULL){
+        if (currItem != &deletedDataItem) {
+            if (strcmp(currItem->key, key) == 0) {
+                free(currItem->key);
+                free(currItem->value);
+                free(currItem);
+                wc->items[index] = item;
+                return;
+            }
+        }
+        index = getHash(item->key, i, wc->size);
+        currItem = wc->items[index];
+        i++;
+    }
+    wc->items[index] = item;
+    wc->filledCount++;
+};
+
+void delete(struct wc* wc, const char* key) {
+    int index = getHash(key, 0, wc->size);
+    dataItem* item = wc->items[index];
+    int i = 1;
+    while (item != NULL) {
+        if (item != &deletedDataItem) {
+            if (strcmp(item->key, key) == 0) {
+                free(item->key);
+                free(item->value);
+                free(item);
+                wc->items[index] = &deletedDataItem;
+            }
+        }
+        index = getHash(key, i, wc->size);
+        item = wc->items[index];
+        i++;
+    } 
+    wc->filledCount--;
 }
 
-unsigned long
-hash(unsigned char *str)
-{
-    unsigned long hash = 5381;
-    int c;
-
-    while ((c = *str++))
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
-    return hash;
+char* search(struct wc* wc, const char* key) {
+    int index = getHash(key, 0, wc->size);
+    dataItem* item = wc->items[index];
+    int i = 1;
+    while (item != NULL) {
+        if (strcmp(item->key, key) == 0) {
+            return item->value;
+        }
+        index = getHash(key, i, wc->size);
+        item = wc->items[index];
+        i++;
+    } 
+    return NULL;
 }
-
-// struct wc *search(int key){
-// 	hashedArray
-// };
-
-struct dataItem *search(int key) {
-   //get the hash 
-   int hashIndex = hashCode(key);  
-	
-   //move in array until an empty 
-   while(wc[hashIndex] != NULL) {
-	
-      if(wc[hashIndex]->key == key)
-         return wc[hashIndex]; 
-			
-      //go to next cell
-      ++hashIndex;
-		
-      //wrap around the table
-      hashIndex %= SIZE;
-   }        
-	
-   return NULL;        
-}
-
-// void insert(int key, int data) {
-//    struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
-//    item->data = data;  
-//    item->key = key;
-
-//    //get the hash 
-//    int hashIndex = hashCode(key);
-
-//    //move in array until an empty or deleted cell
-//    while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->key != -1) {
-//       //go to next cell
-//       ++hashIndex;
-		
-//       //wrap around the table
-//       hashIndex %= SIZE;
-//    }
-	
-//    hashArray[hashIndex] = item;
-// }
 
 struct wc *
 wc_init(char *word_array, long size)
-{
-	// struct wc *wc;
-	unsigned char word[256] = "";
-	for(int i = 0; i < size; i++){
-		if(isspace(word_array[i]) == 0){
-			word[sizeof(word)] = word_array[i];
-		}
-		else{
-			printf(&search(hash(word)));
-			memset(word,0,sizeof(word));
-		}
-		
-	};
-	return &wc;
+{   
+    struct wc* wc = (struct wc *)malloc(sizeof(struct wc));
+    wc->size = 4000;
+    wc->filledCount = 0;
+    wc->items = calloc((size_t)wc->size, sizeof(dataItem*));
+    char word[40]= "";
+    for(int i = 0; i < size; i++){
+            if(isspace(word_array[i]) == 0){
+                word[strlen(word)] = word_array[i];
+            }
+            else{
+                char* searchRes = search(wc, word);
+                if(searchRes == NULL){
+                    insert(wc, word, "1");
+                }
+                else{
+                    int val = atoi(searchRes);
+                    val++;
+                    sprintf(searchRes,"%d", val);
+                    insert(wc, word, searchRes);
+                }
+                memset(word,0,strlen(word));
+            }
+
+    };
+    return wc;
 }
 
 void
 wc_output(struct wc *wc)
 {
-	printf("here output");
+    for(int i=0; i < wc->size; i++){
+        if(wc->items[i] != NULL){
+            printf(wc->items[i]->key);
+            printf(":");
+            printf(wc->items[i]->value);
+            printf("\n");
+        }
+    }
 }
 
 void
 wc_destroy(struct wc *wc)
 {
-	printf("here output");
-	free(wc);
+    for (int i = 0; i < wc->size; i++){
+        dataItem* item = wc->items[i];
+        if(item != NULL){
+            free(item->key);
+            free(item->value);
+            free(item);
+        }
+    }
+    free(wc->items);
+    free(wc);
 }
